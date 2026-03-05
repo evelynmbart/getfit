d5e1ce559283d088567315a3eca5b2fad139e803
Evelyn Bart <evelyn.m.bart@gmail.com>
2026-03-05 11:58:20 -0500

    flesh out sign up/login page layout

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,5 +1,40 @@
+import { useState } from "react";
+
 function Auth() {
-  return <div>login or sign up</div>;
+  const [isLogin, setIsLogin] = useState<boolean>(true);
+  // login should be default route
+  // if user clicks 'sign up' button, hide login form and show sign up form
+  return (
+    <section className="auth">
+      <h1 className="auth__title">{isLogin ? "Login" : "Sign up"}</h1>
+      {isLogin ? (
+        <form className="auth__form">
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+          <p className="auth__signup">
+            Don't have an account? Click here to{" "}
+            <a href="#" onClick={() => setIsLogin(false)}>
+              Sign up
+            </a>
+          </p>
+        </form>
+      ) : (
+        <form className="auth__form">
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Confirm Password" />
+          <button type="submit">Sign up</button>
+          <p className="auth__signup">
+            Already have an account? Click here to{" "}
+            <a href="#" onClick={() => setIsLogin(true)}>
+              Login
+            </a>
+          </p>
+        </form>
+      )}
+    </section>
+  );
 }
 
 export default Auth;
